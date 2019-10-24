0d2aa651ef20494bd7c1856f5142b54f515d2996
matheusguimaraes1 <matheusrg21@hotmail.com>
2019-10-24 10:55:40 -0300

    Primeiro commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+#define NUM_PESSOAS       20
+#define MAX_CADEIRA_BOA    5
+#define MAX_CADEIRA_MEDIA  3
+#define MAX_CADEIRA_RUIM   9
+
+
+int main(){
+    
+    return 0;
+}
